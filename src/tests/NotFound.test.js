479b551cc52d7f0cd6242105a13d26610c19d8ba
HigoJhon <higojhon98@gmail.com>
2022-11-22 20:32:54 -0300

    Criando test para o componente <NotFound.js />

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+test('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+  renderWithRouter(<NotFound />);
+
+  const textH2 = screen.getByRole('heading', {
+    name: /page requested not found/i,
+  });
+  expect(textH2).toBeInTheDocument();
+});
+test('Teste se a página mostra a imagem', () => {
+  renderWithRouter(<NotFound />);
+  const imagem = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+  const img = screen.getByRole('img', {
+    name: /pikachu crying because the page requested was not found/i,
+  });
+  expect(img).toBeInTheDocument();
+  expect(img.src).toBe(imagem);
+});
